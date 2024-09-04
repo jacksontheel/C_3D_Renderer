83f9ca28ab96c458022f9bade361abac481c4645
Jackson Theel <jackson.l.theel@gmail.com>
2024-09-03 18:21:43 -0600

    using dynamic arrays

--- src/main.c
+++ src/main.c
@@ -5,17 +5,16 @@
 #include "display.h"
 #include "vector.h"
 #include "mesh.h"
+#include "array.h"
 
-triangle_t triangles_to_render[N_MESH_FACES];
+triangle_t* triangles_to_render;
 
 vec3_t camera_position = { 0, 0, -2.5 };
-vec3_t cube_rotation = { .x = 0, .y = 0, .z = 0 };
-
-float fov_factor = 240;
 
 bool is_running = false;
-
 int previous_frame_time = 0;
+
+float fov_factor = 240;
 
 void setup(void) {
     color_buffer = (uint32_t*) malloc(sizeof(uint32_t) * window_width * window_height);
@@ -27,6 +26,8 @@ void setup(void) {
         window_width,
         window_height
     );
+
+    load_cube_mesh_data();
 }
 
 void process_input(void) {
@@ -62,25 +63,27 @@ void update(void) {
     
     previous_frame_time = SDL_GetTicks();
 
-    cube_rotation.x += 0.01;
-    cube_rotation.y += 0.01;
-    cube_rotation.z += 0.01;
+    triangles_to_render = NULL;
+
+    mesh.rotation.x += 0.01;
+    mesh.rotation.y += 0.01;
+    mesh.rotation.z += 0.01;
 
     triangle_t projected_triangle;
-    for (int i = 0; i < N_MESH_FACES; i++) {
-        face_t mesh_face = mesh_faces[i];
+    for (int i = 0; i < array_length(mesh.faces); i++) {
+        face_t mesh_face = mesh.faces[i];
 
         vec3_t face_vertices[3];
-        face_vertices[0] = mesh_vertices[mesh_face.a - 1];
-        face_vertices[1] = mesh_vertices[mesh_face.b - 1];
-        face_vertices[2] = mesh_vertices[mesh_face.c - 1];
+        face_vertices[0] = mesh.vertices[mesh_face.a - 1];
+        face_vertices[1] = mesh.vertices[mesh_face.b - 1];
+        face_vertices[2] = mesh.vertices[mesh_face.c - 1];
 
         for (int j = 0; j < 3; j++) {
             vec3_t transformed_vertex = face_vertices[j];
 
-            transformed_vertex = vec3_rotate_x(transformed_vertex, cube_rotation.x);
-            transformed_vertex = vec3_rotate_y(transformed_vertex, cube_rotation.y);
-            transformed_vertex = vec3_rotate_z(transformed_vertex, cube_rotation.z);
+            transformed_vertex = vec3_rotate_x(transformed_vertex, mesh.rotation.x);
+            transformed_vertex = vec3_rotate_y(transformed_vertex, mesh.rotation.y);
+            transformed_vertex = vec3_rotate_z(transformed_vertex, mesh.rotation.z);
 
             transformed_vertex.z -= camera_position.z;
 
@@ -91,7 +94,7 @@ void update(void) {
             projected_triangle.points[j] = projected_point;
         }
 
-        triangles_to_render[i] = projected_triangle;
+        array_push(triangles_to_render, projected_triangle);
     }
 
 }
@@ -99,7 +102,8 @@ void update(void) {
 void render(void) {
     draw_grid();
 
-    for (int i = 0; i < N_MESH_FACES; i++) {
+    int  num_triangles = array_length(triangles_to_render);
+    for (int i = 0; i < num_triangles; i++) {
         triangle_t triangle = triangles_to_render[i];
         draw_rect(triangle.points[0].x, triangle.points[0].y, 3, 3, 0xFFFFFF00);
         draw_rect(triangle.points[1].x, triangle.points[1].y, 3, 3, 0xFFFFFF00);
@@ -116,10 +120,17 @@ void render(void) {
         );
     }
 
+    array_free(triangles_to_render);
+
     render_color_buffer();
     clear_color_buffer(0xFF222222);
 
     SDL_RenderPresent(renderer);
+}
+
+void free_resources(void) {
+    array_free(mesh.vertices);
+    array_free(mesh.faces);
 }
 
 int main(void) {
@@ -135,6 +146,7 @@ int main(void) {
     }
 
     destroy_window();
+    free_resources();
 
     return 0;
 }
--- src/mesh.h
+++ src/mesh.h
@@ -4,10 +4,20 @@
 #include "vector.h"
 #include "triangle.h"
 
-#define N_MESH_VERTICES 8
-extern vec3_t mesh_vertices[N_MESH_VERTICES];
+typedef struct {
+    vec3_t* vertices;
+    face_t* faces;
+    vec3_t rotation;
+} mesh_t;
 
-#define N_MESH_FACES (6 * 2)
-extern face_t mesh_faces[N_MESH_FACES];
+extern mesh_t mesh;
+
+#define N_CUBE_VERTICES 8
+extern vec3_t cube_vertices[N_CUBE_VERTICES];
+
+#define N_CUBE_FACES (6 * 2)
+extern face_t cube_faces[N_CUBE_FACES];
+
+void load_cube_mesh_data(void);
 
 #endif
